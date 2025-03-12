52ed85dc0bf0369ee7a720d5f764672dead10cec
Nicolas Pierrot <nicolas@archipels.io>
2025-03-12 15:10:55 +0100

    Add initial project setup with .gitignore, README, and Foundry configuration
    - Created .gitignore to exclude build artifacts and sensitive files
    - Added .gitmodules for submodule dependencies
    - Introduced README with project overview and usage instructions
    - Configured foundry.toml for project structure
    - Implemented initial ERC20 contract with minting functionality

---
+++ src/Money.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+// Imports
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+// Contracts
+
+contract IABD2 is ERC20, Ownable {
+    // Types
+
+    //Attributs
+    // Storage
+    address private admin;
+    // Events
+    // Errors
+
+    error IncorrectEtherAmount(uint256 etherAmount);
+
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) ERC20(name_, symbol_) Ownable(msg.sender) {}
+
+    function mint(address target_, uint256 value_) external onlyOwner {
+        _mint(target_, value_);
+    }
+
+    function payToMint(address target_) external payable {
+        require(msg.value == 1 ether, IncorrectEtherAmount(msg.value));
+        _mint(target_, 100);
+    }
+}
